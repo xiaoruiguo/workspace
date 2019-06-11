6d516923ebea1a368dcaa9c6a7d842e327787577
lijiaocn <lijiaocn@foxmail.com>
2019-06-11 11:40:27 +0800

    add wechat

--- vim-config/.vim/template/=template=.go
+++ vim-config/.vim/template/=template=.go
@@ -1,6 +1,6 @@
 // Create: %YEAR%/%MONTH%/%DAY% %TIME%:00 Change: %YEAR%/%MONTH%/%DAY% %TIME%:00
 // FileName: %FFILE%
-// Copyright (C) %YEAR% %USER% <%MAIL%>
+// Copyright (C) %YEAR% %USER% <%MAIL%> wechat:lijiaocn 
 //
 // Distributed under terms of the %LICENSE% license.
 
