8341e7b6df82440129a2cc3537fefd1f178636ea
lijiaocn <lijiaocn@foxmail.com>
2019-11-20 17:28:46 +0800

    m

--- studys/study-Nginx/demo/start_nginx.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/sh
-#
-# start_nginx.sh
-# Copyright (C) 2019 lijiaocn <lijiaocn@foxmail.com wechat:lijiaocn>
-#
-# Distributed under terms of the GPL license.
-#
-
-brew services restart nginx
--- studys/study-Nginx/start_backends.sh
+++ studys/study-Nginx/start_backends.sh
@@ -7,7 +7,7 @@
 #
 
 docker rm -f echoserver
-docker run -idt --name echoserver -p 9090:8080 googlecontainer/echoserver:1.10
+docker run -idt --name echoserver -p 9090:8080 -p 9443:8443 googlecontainer/echoserver:1.10
 
 docker rm -f http-record
 docker run -idt --name http-record -p 9091:8080 lijiaocn/http-record:0.0.1
